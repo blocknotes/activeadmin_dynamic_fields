a172192eb73102822090fa50993b1bfeab93e09a
Mattia Roccoberton <mat@blocknot.es>
2019-06-15 20:42:32 +0200

    🎉 Support ActiveAdmin 2.x and code style improvements
    - Add support ActiveAdmin for 2.x
    - Add rubocop config
    - Add frozen_string_literal to ruby files
    - Apply changes to respect rubocop alerts

--- lib/activeadmin/dynamic_fields.rb
+++ lib/activeadmin/dynamic_fields.rb
@@ -1,1 +1,3 @@
+# frozen_string_literal: true
+
 require 'activeadmin/dynamic_fields/engine'
--- lib/activeadmin/dynamic_fields/version.rb
+++ lib/activeadmin/dynamic_fields/version.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module ActiveAdmin
   module DynamicFields
-    VERSION = '0.2.2'
+    VERSION = '0.2.6'
   end
 end
