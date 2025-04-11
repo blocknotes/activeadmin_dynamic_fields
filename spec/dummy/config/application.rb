45e67f38761b36ae0c94eb32ab4d18d77e6ec120
Mattia Roccoberton <mat@blocknot.es>
2025-04-11 08:48:25 +0200

    build: Update dev setup·

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -6,13 +6,7 @@ Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0 if Rails::VERSION::MAJOR == 6
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.load_defaults Rails::VERSION::STRING.to_f
 
     config.active_record.legacy_connection_handling = false if Gem::Version.new(Rails.version) < Gem::Version.new('7.1')
 
